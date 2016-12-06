bd365fe9d8bed62411a17b9c0be6116fd13e7f4f
GabrielDaltro <gabriel.d.8@hotmail.com>
2016-12-06 18:25:15 -0300

    Add files via upload

---
+++ sequeciaLEDs.sv
@@ -0,0 +1,24 @@
+/*
+IFPB
+Joao Pessoa, 02 de dezembro de 2016
+Aluno: Gabriel Daltro Duarte
+
+ Disciplina System Verilog - Programa de Execelencia em Microeletronica
+
+ Exercicio:
+4) Fazer circuito que acenda os LEDs em sequência. Exemplo com 4 LEDs:
+
+L0 L1 L2 L3
+
+ *
+      *
+          *
+               *
+          *
+      *
+ *
+      *
+(...)
+
+
+*/
